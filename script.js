c1f0a0e1a1bdbdab3678d720b1dff6ce3c8fe566
salman <70580281+salman9011@users.noreply.github.com>
2023-12-20 23:55:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 // script.js
+ const form = document.getElementById("form1");
 
 function getFormvalue() {
     // Get the form element by its ID
-    var form = document.getElementById("form1");
-
-    // Access the input elements by their names
-    var firstName = form.elements["fname"].value;
-    var lastName = form.elements["lname"].value;
-
-    // Display the values using alert
-    alert("First Name: " + firstName + "Last Name: " + lastName);
+let fullName = form.["fname"].value +" " + form.["lname"].value;
+	alert(fullName);
 
     // Prevent the form from actually submitting
 }
